e4b690a45f7a607369d844b4d055aa26afaaaeac
tatendabakozw <tatendabakozw@gmail.com>
2021-07-06 11:19:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-200">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
