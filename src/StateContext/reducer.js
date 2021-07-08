a5aea6a4fe3e1d55a3a868a1981a8b9faab1854d
tatendabakozw <tatendabakozw@gmail.com>
2021-07-08 13:01:39 +0200

    initial state

--- src/StateContext/reducer.js
+++ src/StateContext/reducer.js
@@ -1,54 +1,13 @@
 export const initialState = {
-    user: null,
-    basket: [],
-    desc: null,
-    search: '',
-}
-
-//to get the total from the basket
-export const getBasketTotal = (basket) => {
-    let total = 0
-    // eslint-disable-next-line
-    basket.map(elem => {
-        total = total + elem.price
-    })
-
-    return total
+    view: 'false'
 }
 
 function reducer(state, action) {
     switch (action.type) {
-        case 'SET_USER':
+        case 'SET_NAVBAR':
             return {
                 ...state,
-                user: action.user
-            }
-        case 'SET_SEARCH':
-            return {
-                ...state,
-                search: action.search
-            }
-        case 'SET_DESC':
-            return {
-                ...state,
-                desc: action.desc
-            }
-        case 'ADD_TO_BASKET':
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
-            }
-        case 'REMOVE_FROM_BASKET':
-            let newBasket = [...state.basket];
-            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
-            if (index >= 0) {
-                newBasket.splice(index, 1);
-            } else {
-                console.ward('no item with such id exists in cart')
-            }
-            return {
-                ...state,
-                basket: newBasket
+                view: action.view
             }
         default:
             return state
